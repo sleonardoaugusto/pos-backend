018f1414cac70e68b321e04d96a6578fa043b07a
Leonardo Augusto <sleonardoaugusto@gmail.com>
2021-07-10 17:22:03 -0300

    Fix override db

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Generator
 
 import pytest
@@ -5,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from starlette.testclient import TestClient
 
+from app.api.deps import get_db
 from app.db import Base
 from main import app
 
@@ -15,8 +17,17 @@ engine = create_engine(
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
 @pytest.fixture
 def client() -> Generator:
+    app.dependency_overrides[get_db] = override_get_db
     with TestClient(app) as c:
         yield c
 
@@ -25,3 +36,8 @@ def client() -> Generator:
 def db() -> Generator:
     Base.metadata.create_all(bind=engine)
     yield TestingSessionLocal()
+    _delete_database()
+
+
+def _delete_database():
+    Path(Path().parent).joinpath('test.db').unlink()
