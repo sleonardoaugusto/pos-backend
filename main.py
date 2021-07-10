51b729d6b2de6651ad63a892cb6ca887f4b63fb5
Leonardo Augusto <sleonardoaugusto@gmail.com>
2021-07-10 10:38:45 -0300

    Database migrations lib implemented

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from alembic.config import Config
 from fastapi import FastAPI
 
+from alembic import command
 from app import db
 from app.api import api_router
 from app.db.session import engine
 
 db.Base.metadata.create_all(bind=engine)
+
+alembic_cfg = Config("alembic.ini")
+command.stamp(alembic_cfg, "head")
 
 app = FastAPI()
 
